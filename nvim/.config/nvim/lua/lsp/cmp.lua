e2eea366b20ce373489566578e32e9dad59a2c6f
mohammk9 <mohammk9@cisco.com>
2021-12-16 18:16:35 +0530

    Fix some more related stuff

--- nvim/.config/nvim/lua/lsp/cmp.lua
+++ nvim/.config/nvim/lua/lsp/cmp.lua
@@ -1,25 +1,48 @@
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp = require'cmp'
+local luasnip = require("luasnip")
+
 
 cmp.setup({
+    snippet = {
+        expand = function(args)
+            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+        end,
+    },
     mapping = {
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.close(),
-      ['<Tab>'] = function(fallback)
+      ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_next_item()
+              cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+          elseif has_words_before() then
+              cmp.complete()
           else
-            fallback()
+              fallback()
           end
-      end,
-      ['<S-Tab>'] = function(fallback)
+      end, { "i", "s" }),
+
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_prev_item()
+              cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
           else
-            fallback()
+              fallback()
           end
-        end,
+      end, { "i", "s" }),
+
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
     sources = {
