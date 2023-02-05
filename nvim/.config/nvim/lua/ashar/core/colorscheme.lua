6c4a16eeac0890094380b13e9ddeb3c63eb6f531
Mohammad Ashar Khan <mohammk9@cisco.com>
2023-02-05 13:13:16 +0530

    Use nighfly Diagonistics color option

--- nvim/.config/nvim/lua/ashar/core/colorscheme.lua
+++ nvim/.config/nvim/lua/ashar/core/colorscheme.lua
@@ -1,15 +1,4 @@
--- DiagnosticVirtualText colors
-local custom_highlight = vim.api.nvim_create_augroup("CustomHighlight", {})
-vim.api.nvim_create_autocmd("ColorScheme", {
-    pattern = "nightfly",
-    callback = function()
-        vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { link = "NightflyRed" })
-        vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { link = "NightflyYellow" })
-        vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { link = "NightflyBlue" })
-        vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { link = "NightflyWhite" })
-    end,
-    group = custom_highlight,
-})
+vim.g.nightflyVirtualTextColor = true
 local status, _ = pcall(vim.cmd, "colorscheme nightfly")
 if not status then
     vim.notify("Coloscheme not found")
