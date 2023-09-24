775cf4b4b1a99bbc82db72e85af2b960d6cfd694
coder3101 <ashar786khan@gmail.com>
2023-09-24 23:27:46 +0530

    fix: remove deprecated hide_root_folder option

--- nvim/.config/nvim/lua/ashar/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/ashar/plugins/nvim-tree.lua
@@ -46,7 +46,6 @@ nvimtree.setup({
 
     view = {
         width = 30,
-        hide_root_folder = false,
         side = "left",
         preserve_window_proportions = false,
         number = false,
