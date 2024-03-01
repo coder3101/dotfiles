8eba41fadc40bf5bc989bc72b22c7a1dad9cc00c
mohammadkhan <mohammadkhan@digitalocean.com>
2024-03-01 20:47:17 +0530

    ai: add gen.nvim using mistral

--- nvim/.config/nvim/lua/ashar/plugins/llmgen.lua
+++ nvim/.config/nvim/lua/ashar/plugins/llmgen.lua
@@ -1,11 +1,11 @@
 require('gen').setup({
-    model = "mistral", -- The default model to use.
-    host = "localhost",            -- The host running the Ollama service.
-    port = "11434",                -- The port on which the Ollama service is listening.
-    display_mode = "float",        -- The display mode. Can be "float" or "split".
-    show_prompt = false,           -- Shows the Prompt submitted to Ollama.
-    show_model = false,            -- Displays which model you are using at the beginning of your chat session.
-    no_auto_close = false,         -- Never closes the window automatically.
+    model = "mistral",      -- The default model to use.
+    host = "localhost",     -- The host running the Ollama service.
+    port = "11434",         -- The port on which the Ollama service is listening.
+    display_mode = "split", -- The display mode. Can be "float" or "split".
+    show_prompt = true,    -- Shows the Prompt submitted to Ollama.
+    show_model = true,     -- Displays which model you are using at the beginning of your chat session.
+    no_auto_close = false,  -- Never closes the window automatically.
     init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
     -- Function to initialize Ollama
     command = function(options)
@@ -19,3 +19,6 @@ require('gen').setup({
     -- list_models = '<omitted lua function>', -- Retrieves a list of model names
     debug = false -- Prints errors and the command which is run.
 })
+
+-- Set <leader>ai as :Gen<CR> in visual mode
+vim.api.nvim_set_keymap('v', "<leader>ai", ":Gen<CR>", { silent = true, noremap = true })
