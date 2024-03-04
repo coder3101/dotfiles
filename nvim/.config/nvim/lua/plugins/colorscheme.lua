575d16195a8fa2edbbcd4f2a7823e6f005ddf534
mohammadkhan <mohammadkhan@digitalocean.com>
2024-03-04 22:06:30 +0530

    ai: add llm-ls and hugging-face llm extension

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,45 @@
-return { -- You can easily change to a different colorscheme.
-	-- Change the name of the colorscheme plugin below, and then
-	-- change the command in the config to whatever the name of that colorscheme is
-	--
-	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-	"bluz71/vim-nightfly-guicolors",
+return {
+	"folke/tokyonight.nvim",
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
+	style = "night",
 	config = function()
-		-- Load the colorscheme here
-		vim.cmd.colorscheme("nightfly")
-
-		-- You can configure highlights by doing something like
-		vim.cmd.hi("Comment gui=none")
+		vim.cmd.colorscheme("tokyonight-night")
 	end,
+	opts = {
+		on_highlights = function(hl, c)
+			local prompt = "#2d3149"
+			hl.TelescopeNormal = {
+				bg = c.bg_dark,
+				fg = c.fg_dark,
+			}
+			hl.TelescopeBorder = {
+				bg = c.bg_dark,
+				fg = c.bg_dark,
+			}
+			hl.TelescopePromptNormal = {
+				bg = prompt,
+			}
+			hl.TelescopePromptBorder = {
+				bg = prompt,
+				fg = prompt,
+			}
+			hl.TelescopePromptTitle = {
+				bg = prompt,
+				fg = prompt,
+			}
+			hl.TelescopePreviewTitle = {
+				bg = c.bg_dark,
+				fg = c.bg_dark,
+			}
+			hl.TelescopeResultsTitle = {
+				bg = c.bg_dark,
+				fg = c.bg_dark,
+			}
+		end,
+		on_colors = function(colors)
+			colors.hint = colors.orange
+			colors.error = "#ff0000"
+		end,
+	},
 }
