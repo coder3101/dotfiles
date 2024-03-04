1c0ef8f57c810efc357a0a4bd24a4b34a7878279
mohammadkhan <mohammadkhan@digitalocean.com>
2024-03-04 21:20:53 +0530

    cmp: Use menuselect and not menuinsert

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -52,7 +52,7 @@ return { -- Autocompletion
 					luasnip.lsp_expand(args.body)
 				end,
 			},
-			completion = { completeopt = "menu,menuone,noinsert" },
+			completion = { completeopt = "menu,menuone,noselect" },
 			preselect = cmp.PreselectMode.None,
 
 			-- For an understanding of why these mappings were
@@ -61,14 +61,14 @@ return { -- Autocompletion
 			-- No, but seriously. Please read `:help ins-completion`, it is really good!
 			mapping = cmp.mapping.preset.insert({
 				-- Select the [n]ext item
-				-- ['<C-n>'] = cmp.mapping.select_next_item(),
-				-- -- Select the [p]revious item
-				-- ['<C-p>'] = cmp.mapping.select_prev_item(),
-				--
-				-- -- Accept ([y]es) the completion.
-				-- --  This will auto-import if your LSP supports it.
-				-- --  This will expand snippets if the LSP sent a snippet.
-				-- ['<C-y>'] = cmp.mapping.confirm { select = true },
+				-- ["<C-n>"] = cmp.mapping.select_next_item(),
+				-- Select the [p]revious item
+				-- ["<C-p>"] = cmp.mapping.select_prev_item(),
+
+				-- Accept ([y]es) the completion.
+				--  This will auto-import if your LSP supports it.
+				--  This will expand snippets if the LSP sent a snippet.
+				-- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				--
 				-- -- Manually trigger a completion from nvim-cmp.
 				-- --  Generally you don't need this, because nvim-cmp will display
