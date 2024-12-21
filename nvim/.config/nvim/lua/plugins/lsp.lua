496f5fbfa36dc222545acb75b32132c0f0a78028
coder3101 <ashar786khan@gmail.com>
2024-12-21 22:20:12 +0530

    Use ensure installed from mason-lspconfig

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -214,20 +214,17 @@ return { -- LSP Configuration & Plugins
 		}
 
 		require("mason").setup()
-
-		local ensure_installed = vim.tbl_keys(servers or {})
-		vim.list_extend(ensure_installed, {
-			"stylua", -- Used to format lua code
-		})
-		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-		require("mason-lspconfig").setup()
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				local server = servers[server_name] or {}
-				server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-				require("lspconfig")[server_name].setup(server)
-			end,
+		require("mason-tool-installer").setup({ ensure_installed = { "stylua", "protolint", "revive" } })
+		require("mason-lspconfig").setup({
+			ensure_installed = vim.tbl_keys(servers or {}),
+			automatic_installation = false,
+			handlers = {
+				function(server_name)
+					local server = servers[server_name] or {}
+					server.capabilities = capabilities
+					require("lspconfig")[server_name].setup(server)
+				end,
+			},
 		})
 		require("lspsaga").setup({
 			lightbulb = {
