e9a6c98c9635ed495cd43fbaa8b81c31cdbe9a87
coder3101 <ashar786khan@gmail.com>
2025-04-05 01:58:27 +0530

    feat: add mountain as term background

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,10 +5,10 @@ local config = {}
 config.font = wezterm.font("Monaspace Neon")
 config.font_size = 16.0
 config.color_scheme = "Catppuccin Macchiato"
-config.window_background_image = os.getenv("HOME") .. "/.config/wezterm/images/quran.jpg"
+config.window_background_image = os.getenv("HOME") .. "/.config/wezterm/images/mountain.jpg"
 config.window_background_opacity = 0.95
 config.window_background_image_hsb = {
-	brightness = 0.03,
+	brightness = 0.3,
 }
 config.text_background_opacity = 0.75
 config.window_padding = {
