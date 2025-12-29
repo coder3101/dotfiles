f5dbf6f6d854e9bf1c5e0bac1b9e7a1d9ac2aba5
mohammadkhan <mohammadkhan@digitalocean.com>
2025-12-29 18:45:10 +0530

    use: neotest

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -6,8 +6,8 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		"antoinemadec/FixCursorHold.nvim",
 		{ "fredrikaverpil/neotest-golang", version = "*" },
-		-- "rouge8/neotest-rust",
-		{ "mrcjkb/rustaceanvim", version = "^6", lazy = false },
+		"rouge8/neotest-rust",
+		-- { "mrcjkb/rustaceanvim", version = "^6", lazy = false },
 		"nvim-neotest/neotest-python",
 	},
 	config = function()
@@ -22,8 +22,8 @@ return {
 					go_test_args = { "-v", "-race", "-count=1", "-timeout=90s" },
 				}),
 				require("neotest-python"),
-				-- require("neotest-rust"),
-				require("rustaceanvim.neotest"),
+				require("neotest-rust"),
+				-- require("rustaceanvim.neotest"),
 			},
 		})
 		vim.keymap.set("n", "<leader>ts", "<cmd>Neotest summary<cr>", { desc = "[T]est [Summary]" })
